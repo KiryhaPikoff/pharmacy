9a0026a9f4650c1d4fb0d23c2af1da5e05972190
Kiryha <pikov_kirya@mail.ru>
2020-04-14 23:13:07 +0400

    Binding models in MedicamentEjb

---
+++ pmmsl/src/main/java/com/ulstu/pharmacy/pmmsl/medicament/binding/MedicamentBindingModel.java
@@ -0,0 +1,4 @@
+package com.ulstu.pharmacy.pmmsl.medicament.binding;
+
+public class MedicamentBindingModel {
+}
--- pmmsl/src/main/java/com/ulstu/pharmacy/pmmsl/medicament/ejb/MedicamentEjbImpl.java
+++ pmmsl/src/main/java/com/ulstu/pharmacy/pmmsl/medicament/ejb/MedicamentEjbImpl.java
@@ -1,6 +1,7 @@
 package com.ulstu.pharmacy.pmmsl.medicament.ejb;
 
 import com.ulstu.pharmacy.pmmsl.common.exception.CrudOperationException;
+import com.ulstu.pharmacy.pmmsl.medicament.binding.MedicamentBindingModel;
 import com.ulstu.pharmacy.pmmsl.medicament.dao.MedicamentDao;
 import com.ulstu.pharmacy.pmmsl.medicament.entity.Medicament;
 import com.ulstu.pharmacy.pmmsl.medicament.mapper.MedicamentMapper;
@@ -8,10 +9,8 @@ import com.ulstu.pharmacy.pmmsl.medicament.view.MedicamentViewModel;
 import lombok.AllArgsConstructor;
 
 import javax.inject.Inject;
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor(onConstructor = @__({@Inject}))
@@ -44,44 +43,26 @@ public class MedicamentEjbImpl implements MedicamentEjbRemote {
     }
 
     /**
-     * Метод создания медикамента.
+     * Метод предназначени для сохранения (если id = null) или
+     * обновления (id отличен от null) медикамента.
      *
-     * @param name              название медикамента.
-     * @param description       описание медикамента.
-     * @param contraindications противопоказания.
-     * @param instruction       инструкция по применению.
-     * @param price             цена медикамнта.
+     * @param medicamentBindingModel
      */
     @Override
-    public void create(String name,
-                       String description,
-                       String contraindications,
-                       String instruction,
-                       BigDecimal price) throws CrudOperationException {
-        String validationErrors = this.validateCreateArgs(name, description, contraindications, instruction, price);
-        if(Objects.nonNull(validationErrors)) {
-            throw new CrudOperationException(validationErrors);
+    public void addOrUpdate(MedicamentBindingModel medicamentBindingModel) {
+        if(Objects.isNull(medicamentBindingModel.getId())) {
+            String validationErrors = this.validateCreateArgs(medicamentBindingModel);
+            if(Objects.nonNull(validationErrors)) {
+                throw new CrudOperationException(validationErrors);
+            }
+            this.add(medicamentBindingModel);
         } else {
-            this.medicamentDao.save(
-                    Medicament.builder()
-                            .name(name)
-                            .contraindications(contraindications)
-                            .description(description)
-                            .instruction(instruction)
-                            .price(price)
-                            .build()
-            );
+            String validationErrors = this.validateUpdateArgs(medicamentBindingModel);
+            if(Objects.nonNull(validationErrors)) {
+                throw new CrudOperationException(validationErrors);
+            }
+            this.update(medicamentBindingModel);
         }
-    }
-
-    /**
-     * Метод изменения медикамента.
-     *
-     * @param medicamentViewModel
-     */
-    @Override
-    public void update(MedicamentViewModel medicamentViewModel) {
-
     }
 
     /**
@@ -92,36 +73,78 @@ public class MedicamentEjbImpl implements MedicamentEjbRemote {
     @Override
     public void delete(Long id) {
 
+        if(Objects.isNull(id)) {
+            throw new CrudOperationException("Id is null");
+        }
+
+        if(!this.medicamentDao.existsById(id)) {
+            throw new CrudOperationException("Medicament with an id = " + id + " not exist!");
+        } else {
+            this.medicamentDao.deleteById(id);
+        }
+    }
+
+    /**
+     * Метод сохранения медикамента в БД.
+     * @param medicamentBindingModel
+     */
+    private void add(MedicamentBindingModel medicamentBindingModel) {
+        this.medicamentDao.save(
+                Medicament.builder()
+                        .name(medicamentBindingModel.getName())
+                        .description(medicamentBindingModel.getDescription())
+                        .instruction(medicamentBindingModel.getInstruction())
+                        .contraindications(medicamentBindingModel.getContraindications())
+                        .price(medicamentBindingModel.getPrice())
+                        .build()
+        );
+    }
+
+    /**
+     * Метод обновления медикамента в БД.
+     * @param medicamentBindingModel
+     */
+    private void update(MedicamentBindingModel medicamentBindingModel) {
+        Medicament updateMedicament = Medicament.builder()
+                .name(medicamentBindingModel.getName())
+                .description(medicamentBindingModel.getDescription())
+                .instruction(medicamentBindingModel.getInstruction())
+                .contraindications(medicamentBindingModel.getContraindications())
+                .price(medicamentBindingModel.getPrice())
+                .build();
+        updateMedicament.setId(medicamentBindingModel.getId());
+        this.medicamentDao.update(updateMedicament);
     }
 
     /**
      * Валидация аргументов для создания медикамента.
-     * @param name
-     * @param description
-     * @param contraindications
-     * @param instruction
-     * @param price
-     * @return
+     * @return если строка null - нет ошибок. В противном случае все ошибки перечислены.
      */
-    private String validateCreateArgs(String name, String description, String contraindications, String instruction, BigDecimal price) {
+    private String validateCreateArgs(MedicamentBindingModel medicamentBindingModel) {
         StringBuilder errors = new StringBuilder();
 
-        if (Objects.isNull(name)) {
+        if (Objects.isNull(medicamentBindingModel.getName())) {
             errors.append("Name is null; ");
+        } else {
+            if(this.medicamentDao.existByName(medicamentBindingModel.getName())) {
+                errors.append("Medicament with a name = ")
+                        .append(medicamentBindingModel.getName())
+                        .append(" already exist; ");
+            }
         }
-        if (Objects.isNull(description)) {
+        if (Objects.isNull(medicamentBindingModel.getDescription())) {
             errors.append("Description is null; ");
         }
-        if (Objects.isNull(contraindications)) {
+        if (Objects.isNull(medicamentBindingModel.getContraindications())) {
             errors.append("Contraindications are null; ");
         }
-        if (Objects.isNull(instruction)) {
+        if (Objects.isNull(medicamentBindingModel.getInstruction())) {
             errors.append("Instruction is null; ");
         }
-        if (Objects.isNull(price)) {
+        if (Objects.isNull(medicamentBindingModel.getPrice())) {
             errors.append("Price is null; ");
         } else {
-            if (price.signum() == -1) {
+            if (medicamentBindingModel.getPrice().signum() == -1) {
                 errors.append("Price is negative ; ");
             }
         }
@@ -129,17 +152,23 @@ public class MedicamentEjbImpl implements MedicamentEjbRemote {
         return errors.toString().isEmpty() ? null : errors.toString();
     }
 
-    private String validateUpdateArgs(Long id, String name, String description, String contraindications, String instruction, BigDecimal price) {
+    /**
+     * Валидация аргументов для изменения медикамента.
+     * @return если строка null - нет ошибок. В противном случае все ошибки перечислены.
+     */
+    private String validateUpdateArgs(MedicamentBindingModel medicamentBindingModel) {
         StringBuilder errors = new StringBuilder();
 
-        if (Objects.isNull(id)) {
+        if (Objects.isNull(medicamentBindingModel.getId())) {
             errors.append("Id is null; ");
         } else {
-            if (!this.medicamentDao.existsById(id)) {
-                errors.append("Medicament with id = " + id + " not exist; ");
+            if (!this.medicamentDao.existsById(medicamentBindingModel.getId())) {
+                errors.append("Medicament with an id = ")
+                        .append(medicamentBindingModel.getId())
+                        .append(" not exist; ");
             }
         }
-        String otherArgsErrors = this.validateCreateArgs(name, description, contraindications, instruction, price);
+        String otherArgsErrors = this.validateCreateArgs(medicamentBindingModel);
         if (Objects.nonNull(otherArgsErrors)) {
             errors.append(otherArgsErrors);
         }
