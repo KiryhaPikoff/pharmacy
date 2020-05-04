b444c4540c385569fb6b796519b81af26180ed53
Kiryha <pikov_kirya@mail.ru>
2020-05-04 23:36:08 +0400

    Some SupplyEjb logic implementation.

--- pmmsl/src/main/java/com/ulstu/pharmacy/pmmsl/supply/ejb/SupplyEjbImpl.java
+++ pmmsl/src/main/java/com/ulstu/pharmacy/pmmsl/supply/ejb/SupplyEjbImpl.java
@@ -1,15 +1,23 @@
 package com.ulstu.pharmacy.pmmsl.supply.ejb;
 
+import com.ulstu.pharmacy.pmmsl.common.exception.CrudOperationException;
+import com.ulstu.pharmacy.pmmsl.medicament.entity.Medicament;
 import com.ulstu.pharmacy.pmmsl.pharmacy.ejb.PharmacyEjbLocal;
+import com.ulstu.pharmacy.pmmsl.pharmacy.entity.Pharmacy;
 import com.ulstu.pharmacy.pmmsl.supply.binding.SupplyBindingModel;
 import com.ulstu.pharmacy.pmmsl.supply.dao.SupplyDao;
+import com.ulstu.pharmacy.pmmsl.supply.entity.Supply;
+import com.ulstu.pharmacy.pmmsl.supply.entity.SupplyMedicament;
 import com.ulstu.pharmacy.pmmsl.supply.mapper.SupplyMapper;
 import com.ulstu.pharmacy.pmmsl.supply.view.SupplyViewModel;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Stateless
 public class SupplyEjbImpl implements SupplyEjbLocal {
@@ -35,8 +43,29 @@ public class SupplyEjbImpl implements SupplyEjbLocal {
      * @param supplyBindingModel
      */
     @Override
+    @Transactional(Transactional.TxType.MANDATORY)
     public void create(SupplyBindingModel supplyBindingModel) {
-
+        //TODO проверка значений
+        // пополнение в аптеке
+        pharmacyEjbLocal.replenishMedicaments(
+                supplyBindingModel.getPharmacyId(),
+                supplyBindingModel.getMedicamentCountSet()
+        );
+        //TODO формирование сущности
+        Supply newSupply = Supply.builder()
+                //TODO рабочее?
+                .pharmacy(Pharmacy.builder().id(supplyBindingModel.getPharmacyId()).build())
+                .date(new Timestamp(System.currentTimeMillis()))
+                .supplyMedicaments(supplyBindingModel.getMedicamentCountSet().stream()
+                        .map(medicamentCountBindingModel -> SupplyMedicament.builder()
+                                .medicament(Medicament.builder().id(medicamentCountBindingModel.getMedicamentId()).build())
+                                .count(medicamentCountBindingModel.getCount())
+                                .build()
+                        ).collect(Collectors.toSet())
+                )
+                .build();
+        // сохранение
+        supplyDao.save(newSupply);
     }
 
     /**
@@ -48,6 +77,20 @@ public class SupplyEjbImpl implements SupplyEjbLocal {
      */
     @Override
     public List<SupplyViewModel> getFromDateToDate(Timestamp fromDate, Timestamp toDate) {
-        return null;
+
+        StringBuilder errors = new StringBuilder();
+        errors.append(Objects.isNull(fromDate) ? "DateFrom is null; "           : "");
+        errors.append(Objects.isNull(toDate)   ? "DateTo is null; "             : "");
+        if(Objects.nonNull(fromDate) && Objects.nonNull(toDate)) {
+            errors.append(fromDate.after(toDate) ? "DateFrom is after DateTo; " : "");
+        }
+
+        if(!errors.toString().isBlank()) {
+            throw new CrudOperationException(errors.toString());
+        }
+
+        return supplyDao.getFromDateToDate(fromDate, toDate).stream()
+                .map(supplyMapper::toViewModel)
+                .collect(Collectors.toList());
     }
 }
