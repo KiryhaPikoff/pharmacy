5d33e074c24e8f61b5af79db77c2ca14d38068cf
Kiryha <pikov_kirya@mail.ru>
2020-06-04 16:01:29 +0400

    Login and logout redrect fix.

--- web/src/main/java/com/ulstu/pharmacy/web/login/LoginBean.java
+++ web/src/main/java/com/ulstu/pharmacy/web/login/LoginBean.java
@@ -6,9 +6,11 @@ import com.ulstu.pharmacy.web.helper.MessagesHelper;
 import com.ulstu.pharmacy.web.helper.SessionHelper;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.SneakyThrows;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
@@ -29,7 +31,8 @@ public class LoginBean {
         this.loginEjb = loginEjb;
     }
 
-    public String validate() {
+    @SneakyThrows
+    public void validate() {
         boolean valid = loginEjb.validate(
                 Authentication.builder()
                         .login(this.login)
@@ -38,16 +41,16 @@ public class LoginBean {
         if (valid) {
             HttpSession session = SessionHelper.getSession();
             session.setAttribute("isAuthorized", true);
-            return "success";
+            FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
         } else {
             MessagesHelper.errorMessage("Неверный логин или пароль");
-            return "fail";
         }
     }
 
-    public String logout() {
+    @SneakyThrows
+    public void logout() {
         HttpSession session = SessionHelper.getSession();
         session.invalidate();
-        return "logout";
+        FacesContext.getCurrentInstance().getExternalContext().redirect("login.xhtml");
     }
 }
